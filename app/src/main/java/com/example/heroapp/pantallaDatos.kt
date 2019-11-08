f40c8a8715d756d3f33f6649ebee7d233c359a4a
rolo7 <50997752+rolo7@users.noreply.github.com>
2019-11-08 11:59:11 -0600

    pantalla de datos

---
+++ app/src/main/java/com/example/heroapp/pantallaDatos.kt
@@ -0,0 +1,12 @@
+package com.example.heroapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class pantallaDatos : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pantalla_datos)
+    }
+}
