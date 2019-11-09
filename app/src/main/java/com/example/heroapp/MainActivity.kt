5f6037089c4fa41c0f6e3b49ecc489c0360aa729
rolo7 <50997752+rolo7@users.noreply.github.com>
2019-11-08 23:28:39 -0600

    programacion de botones buscar y regresar

--- app/src/main/java/com/example/heroapp/MainActivity.kt
+++ app/src/main/java/com/example/heroapp/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.example.heroapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnBuscar.setOnClickListener(){
+            val Intent = Intent(this, pantallaDatos::class.java)
+            startActivity(Intent)
+        }
+
+
+
     }
 
 }
--- app/src/main/java/com/example/heroapp/pantallaDatos.kt
+++ app/src/main/java/com/example/heroapp/pantallaDatos.kt
@@ -1,12 +1,20 @@
 package com.example.heroapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_pantalla_datos.*
 
 class pantallaDatos : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pantalla_datos)
+
+       btnRegresar.setOnClickListener()
+       {
+           val Intent = Intent(this, MainActivity::class.java)
+           startActivity(Intent)
+       }
     }
 }
